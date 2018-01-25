bbcd4ca809a03226d4ed0de7ff49ec7c18dbe544
Adam Soffer <ads1018@gmail.com>
2018-01-24 21:40:57 -0500

    make a unit test that:
    • alice sends 0.1 Ether to bob with hash1
    • bob collects 0.1 Ether with hash1
    • alice sends 0.1 Ether to bob with hash2
    • bob collects 0.1 Ether with hash1
    
    This indeed fails!

--- test/remittance.js
+++ test/remittance.js
@@ -5,10 +5,18 @@ contract('Remittance', function(accounts) {
   let deposit = web3.utils.toWei('2', 'ether')
   let password1 = web3.utils.fromAscii('b9labs')
   let password2 = web3.utils.fromAscii('rules')
-  let hashedPassword = web3.utils.soliditySha3(
+  let hashedPassword1 = web3.utils.soliditySha3(
     { t: 'bytes32', v: password1 },
     { t: 'bytes32', v: password2 }
   )
+
+  let password3 = web3.utils.fromAscii('hello')
+  let password4 = web3.utils.fromAscii('world')
+  let hashedPassword2 = web3.utils.soliditySha3(
+    { t: 'bytes32', v: password3 },
+    { t: 'bytes32', v: password4 }
+  )
+
   let remittance
   let contractBalanceBefore
   let contractBalanceAfter
@@ -18,16 +26,16 @@ contract('Remittance', function(accounts) {
   })
 
   describe('deposit()', async function() {
-    it('should deposit 2 ether to the fund', async function() {
+    it('should send 2 Ether from Alice to Bob with hash 1', async function() {
       contractBalanceBefore = await web3.eth.getBalance(remittance.address)
-      let fundAmountBeforeDeposit = await remittance.funds(hashedPassword)
+      let fundAmountBeforeDeposit = await remittance.funds(hashedPassword1)
 
-      await remittance.deposit(accounts[1], hashedPassword, {
+      await remittance.deposit(accounts[1], hashedPassword1, {
         from: accounts[0],
         value: deposit
       })
 
-      let fundAmountAfterDeposit = await remittance.funds(hashedPassword)
+      let fundAmountAfterDeposit = await remittance.funds(hashedPassword1)
       assert.strictEqual(
         (Number(fundAmountBeforeDeposit[2]) + Number(deposit)).toString(10),
         fundAmountAfterDeposit[2].toString(10)
@@ -44,15 +52,15 @@ contract('Remittance', function(accounts) {
   })
 
   describe('withdraw()', async function() {
-    it('should withdraw 2 ether from the fund', async function() {
+    it('should withdraw 2 ether for bob with hash1', async function() {
       contractBalanceBefore = await web3.eth.getBalance(remittance.address)
-      let fundAmountBeforeWithdrawal = await remittance.funds(hashedPassword)
+      let fundAmountBeforeWithdrawal = await remittance.funds(hashedPassword1)
 
       await remittance.withdraw(password1, password2, {
         from: accounts[1]
       })
 
-      let fundAmountAfterWithdrawal = await remittance.funds(hashedPassword)
+      let fundAmountAfterWithdrawal = await remittance.funds(hashedPassword1)
       assert.strictEqual(
         fundAmountAfterWithdrawal[2].toString(10),
         (Number(fundAmountBeforeWithdrawal[2]) - Number(deposit)).toString(10)
@@ -63,6 +71,41 @@ contract('Remittance', function(accounts) {
       assert.strictEqual(
         deposit.toString(10),
         (contractBalanceBefore - contractBalanceAfter).toString(10)
+      )
+    })
+  })
+
+  describe('deposit()', async function() {
+    it('should send 2 Ether from Alice to Bob with hash 2', async function() {
+      contractBalanceBefore = await web3.eth.getBalance(remittance.address)
+      let fundAmountBeforeDeposit = await remittance.funds(hashedPassword2)
+
+      await remittance.deposit(accounts[1], hashedPassword2, {
+        from: accounts[0],
+        value: deposit
+      })
+
+      let fundAmountAfterDeposit = await remittance.funds(hashedPassword2)
+      assert.strictEqual(
+        (Number(fundAmountBeforeDeposit[2]) + Number(deposit)).toString(10),
+        fundAmountAfterDeposit[2].toString(10)
+      )
+    })
+  })
+
+  describe('withdraw()', async function() {
+    it('should withdraw 2 ether for bob with *hash1* (Xavier - indeed this fails)', async function() {
+      contractBalanceBefore = await web3.eth.getBalance(remittance.address)
+      let fundAmountBeforeWithdrawal = await remittance.funds(hashedPassword1)
+
+      await remittance.withdraw(password1, password2, {
+        from: accounts[1]
+      })
+
+      let fundAmountAfterWithdrawal = await remittance.funds(hashedPassword1)
+      assert.strictEqual(
+        fundAmountAfterWithdrawal[2].toString(10),
+        (Number(fundAmountBeforeWithdrawal[2]) - Number(deposit)).toString(10)
       )
     })
   })
