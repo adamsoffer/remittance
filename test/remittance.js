236d45bdc9e69ddca6891080f036172ea50a1fe8
Adam Soffer <ads1018@gmail.com>
2018-02-09 01:35:01 -0500

    use beforeEach instead of before and check balanace of bob

--- test/remittance.js
+++ test/remittance.js
@@ -23,16 +23,14 @@ contract('Remittance', function(accounts) {
   )
 
   let remittance
-  let contractBalanceBefore
-  let contractBalanceAfter
 
-  before('should deploy Remittance', async function() {
+  beforeEach('should deploy Remittance', async function() {
     remittance = await Remittance.new({ from: accounts[0] })
   })
 
   describe('deposit()', async function() {
     it('should send 2 Ether from Alice to Bob with hash 1', async function() {
-      contractBalanceBefore = await web3.eth.getBalance(remittance.address)
+      let contractBalanceBefore = await web3.eth.getBalance(remittance.address)
       let fundAmountBeforeDeposit = await remittance.funds(hashedPassword1)
       let deadline = Math.floor(Date.now() / 1000) + 3600 // 1 hour from now
       await remittance.deposit(accounts[1], deadline, password1, password2, {
@@ -45,9 +43,11 @@ contract('Remittance', function(accounts) {
         (Number(fundAmountBeforeDeposit[2]) + Number(deposit)).toString(10),
         fundAmountAfterDeposit[2].toString(10)
       )
-    })
-
-    it('should deposit 2 ether to the contract', async function() {
+      let contractBalanceAfter = await web3.eth.getBalance(remittance.address)
+      assert.strictEqual(
+        deposit.toString(10),
+        (contractBalanceAfter - contractBalanceBefore).toString(10)
+      )
       contractBalanceAfter = await web3.eth.getBalance(remittance.address)
       assert.strictEqual(
         deposit.toString(10),
@@ -58,49 +58,70 @@ contract('Remittance', function(accounts) {
 
   describe('withdraw()', async function() {
     it('should withdraw 2 ether for bob with hash1', async function() {
-      contractBalanceBefore = await web3.eth.getBalance(remittance.address)
-      let fundAmountBeforeWithdrawal = await remittance.funds(hashedPassword1)
-
-      await remittance.withdraw(password1, password2, {
-        from: accounts[1]
+      let deadline = Math.floor(Date.now() / 1000) + 3600 // 1 hour from now
+      await remittance.deposit(accounts[1], deadline, password1, password2, {
+        from: accounts[0],
+        value: deposit
       })
+      let contractBalanceBefore = await web3.eth.getBalance(remittance.address)
+      let bobAccountBalanceBefore = await web3.eth.getBalance(accounts[1])
+      let fundAmountBeforeWithdrawal = await remittance.funds(hashedPassword1)
+      let gasPrice = await web3.eth.getGasPrice()
+      let tx = await remittance.withdraw(password1, password2, {
+        from: accounts[1],
+        gas: '1500000',
+        gasPrice
+      })
+
+      gasCost = web3.utils
+        .toBN(gasPrice)
+        .mul(web3.utils.toBN(tx.receipt.gasUsed))
 
       let fundAmountAfterWithdrawal = await remittance.funds(hashedPassword1)
       assert.strictEqual(
         fundAmountAfterWithdrawal[2].toString(10),
         (Number(fundAmountBeforeWithdrawal[2]) - Number(deposit)).toString(10)
       )
-    })
-    it('should withdraw 2 ether from the contract', async function() {
-      contractBalanceAfter = await web3.eth.getBalance(remittance.address)
+      let contractBalanceAfter = await web3.eth.getBalance(remittance.address)
       assert.strictEqual(
         deposit.toString(10),
         (contractBalanceBefore - contractBalanceAfter).toString(10)
+      )
+
+      let bobAccountBalanceAfter = await web3.eth.getBalance(accounts[1])
+
+      assert.strictEqual(
+        web3.utils
+          .toBN(bobAccountBalanceBefore)
+          .add(web3.utils.toBN(deposit))
+          .toString(10),
+        web3.utils
+          .toBN(bobAccountBalanceAfter)
+          .add(gasCost)
+          .toString(10)
       )
     })
   })
 
   describe('reclaim()', async function() {
-    it('should send another 2 Ether from Alice to Bob with hash 1', async function() {
-      contractBalanceBefore = await web3.eth.getBalance(remittance.address)
+    it('should reclaim 2 ether after the deadline passes', async function() {
       let fundAmountBeforeDeposit = await remittance.funds(hashedPassword2)
       let deadline = Math.floor(Date.now() / 1000) + 3600 // 1 hour from now
       await remittance.deposit(accounts[1], deadline, password3, password4, {
         from: accounts[0],
         value: deposit
       })
-
+      let contractBalanceBeforeReclaim = await web3.eth.getBalance(
+        remittance.address
+      )
       let fundAmountAfterDeposit = await remittance.funds(hashedPassword2)
       assert.strictEqual(
         (Number(fundAmountBeforeDeposit[2]) + Number(deposit)).toString(10),
         fundAmountAfterDeposit[2].toString(10)
       )
-    })
 
-    it('should reclaim 2 ether after the deadline passes', async function() {
-      let contractBalanceBefore = await web3.eth.getBalance(remittance.address)
       let fundAmountBeforeReclaim = await remittance.funds(hashedPassword2)
-      let result = await web3.evm.increaseTimePromise(4000)
+      await web3.evm.increaseTimePromise(3600)
       await remittance.reclaim(password3, password4, {
         from: accounts[0]
       })
@@ -109,12 +130,6 @@ contract('Remittance', function(accounts) {
       assert.strictEqual(
         fundAmountAfterReclaim[2].toString(10),
         (Number(fundAmountBeforeReclaim[2]) - Number(deposit)).toString(10)
-      )
-
-      let contractBalanceAfter = await web3.eth.getBalance(remittance.address)
-      assert.strictEqual(
-        deposit.toString(10),
-        (contractBalanceBefore - contractBalanceAfter).toString(10)
       )
     })
   })
